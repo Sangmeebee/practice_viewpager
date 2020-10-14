ce0b5c1215653439de1b6802ff3dd75cad0f44d5
sangminkim <apfhdznzl@gmail.com>
2020-10-15 08:51:27 +0900

    attach tablayout & viewpager

--- viewpager/app/src/main/java/com/sangmee/viewpager/MainActivity.kt
+++ viewpager/app/src/main/java/com/sangmee/viewpager/MainActivity.kt
@@ -2,6 +2,7 @@ package com.sangmee.viewpager
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.tabs.TabLayoutMediator
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -10,5 +11,12 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         viewPager.adapter = FragmentAdapter(this)
+
+        TabLayoutMediator(tl_container, viewPager) { tab, position ->
+            when (position) {
+                0 -> tab.text = "게시물"
+                else -> tab.text = "저장함"
+            }
+        }.attach()
     }
 }
